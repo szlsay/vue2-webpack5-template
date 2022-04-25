1430a7f8590f814e9882234b5d04ec7a4c17fdb0
小沈曰 <409178030@qq.com>
2022-04-25 21:30:51 +0800

    安装webpack

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/main.js'),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, './build'),
+  },
+}
