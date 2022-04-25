51dad9ad58f9e7e887d8ffc3559f27b4d1103656
小沈曰 <409178030@qq.com>
2022-04-25 23:34:11 +0800

    配置webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 module.exports = {
   mode: 'development',
   entry: path.resolve(__dirname, './src/main.js'),
+  plugins: [
+    require('autoprefixer'),
+    new HtmlWebpackPlugin({
+      title: 'webpack',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, './build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: path.resolve(__dirname, 'node_modules'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+    ],
   },
 }
